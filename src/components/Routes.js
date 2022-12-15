28c54edfda41030f8c89e2b360f2707f9cbae087
Juan Alberto R. Santana <116007946+EasyCode0x7@users.noreply.github.com>
2022-12-15 22:57:11 +0100

    Delete src directory

--- src/components/Routes.js
+++
@@ -1,8 +0,0 @@
-<Router>
-    <Route path='/' Component={Home} />
-    <Route path='/' Component={Works} />
-    <Route path='/' Component={Header} />
-    <Route path='/' Component={Footer} />
-    <Route path='/' Component={Error404} />
-</Router>
-
