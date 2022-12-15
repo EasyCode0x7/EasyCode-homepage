aff2ba9461bb095153653331c0b2264ad0d321ba
Juan Alberto R. Santana <116007946+EasyCode0x7@users.noreply.github.com>
2022-12-15 16:58:12 +0100

    Add files via upload

---
+++ src/components/Works.js
@@ -0,0 +1,67 @@
+import '../App.css';
+import React, { Component } from 'react';
+
+
+
+function Works() {
+
+    return(
+        <div className="text-white  fnt-em">
+
+            <div  className='pt-5'>
+                <h2 style={{fontSize: "1.7em"}}>Work</h2>
+                <p className='css-15t5e7o'>
+                Juan is an independent developer and Front-end Developer based in Spain with a passion for creating digital
+                services/things he wants. He has a knack for everything related to product launches, from planning and
+                design to solving real-life problems with code. When he's not online, he loves to hang out hobnobbing and
+                with her family. Currently, he is looking for a job in a company that will make him grow as a professional.
+                    He will post content to market his products and his YouTube channel called "<a className='text-danger text-decoration-none' href='https://www.youtube.com/@EasyCode_'> Easy Code </a>".
+                </p>
+            </div>
+
+            <div class="py-4 d-flex">
+                <a class="btn btn-warning mx-auto" href='/resume' style={{fontSize: "1.2em", fontWeight: "600"}}>My portfolio</a>
+            </div>
+
+            <div className=''>
+                <h2 className='pt-4' style={{fontSize: "1.7em"}}>NextJs</h2>
+                <p className='css-15t5e7o'>
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                </p>
+            </div>
+
+            <div className=''>
+                <h2 className='pt-4' style={{fontSize: "1.7em"}}>ReactJs</h2>
+                <p className='css-15t5e7o'>
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                </p>
+            </div>
+
+            <div className=''>
+                <h2 className='pt-4' style={{fontSize: "1.7em"}}>Bio</h2>
+                <p className='css-15t5e7o'>
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                    I offer from a wide range of services, including system administration, 
+                    programming and ethical hacking.
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Works;
+
