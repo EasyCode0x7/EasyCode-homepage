d947226847e5dfb85e0523bf68c6b9f12140550e
Juan Alberto R. Santana <116007946+EasyCode0x7@users.noreply.github.com>
2022-12-15 22:58:01 +0100

    Add files via upload

---
+++ src/components/Error404.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+
+class Error404 extends React.Component{
+    
+}
+
+
+export default Error404;
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+function Footer(){
+
+    return(
+
+        <div>
+            <p>By: @Denichel</p>
+        </div>
+    )
+}
+
+export default Footer;
